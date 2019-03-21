4636af639610128f92378b7aeb817e51c2bb01ff
kamera <asahi20000124@icloud.com>
2019-03-21 21:59:12 +0900

    vegetableとknifeのタグを追加、野菜回転用のAnimationを追加、Peeler.cs,PeelManeger.cs,PeelVegetable.csを追加

---
+++ Assets/Scripts/Kamera/PeelManeger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PeelManeger : MonoBehaviour
+{
+    private float peelTime = 10, timeCount, peelLimit;
+    private int peelCount;
+    [SerializeField]
+    PeelVegetable peelVeg;
+    [SerializeField]
+    private PeelVegetable[] vegetables;
+    private GameObject[] vegetablesObj;
+
+    [SerializeField]
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        peelCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Peel();
+        PeelView();
+    }
+
+    void Peel()
+    {
+        if (peelVeg.Peeling)
+        {
+            timeCount += Time.deltaTime;
+            if (timeCount >= peelTime)
+            {
+                peelCount++;
+                timeCount = 0;
+                if (peelCount >= peelLimit)
+                {
+                    //おわた
+                    Debug.Log("");
+                }
+            }
+        }
+    }
+
+    void PeelView()
+    {
+        slider.value = timeCount / peelTime;
+    }
+}
