a7679f09b2a0ac09db9af075334cc8489dcc934d
kamera <asahi20000124@icloud.com>
2019-03-22 05:49:05 +0900

    いくつかのタグを追加、レーザーを作成、切れたにんじんのPrefabを追加、レーザーでニンジンを切断できるようにした

---
+++ Assets/Scripts/cut/CutVegetable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutVegetable : MonoBehaviour
+{
+    int cutCount, cutLimit = 3;
+    [SerializeField]
+    private GameObject vegetable, cutVegetable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cutCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Laser")
+        {
+            cutCount++;
+            if (cutLimit <= cutCount)
+            {
+                Instantiate(cutVegetable, vegetable.transform.position, Quaternion.identity);
+                Destroy(vegetable);
+            }
+        }
+    }
+}
