5e518f8f0e14a4c71b60fa03aeb84de400e83ba0
TomyJPN <masaya120331@gmail.com>
2019-03-21 23:02:20 +0900

    レーザーポインタを動かすスクリプトを実装

---
+++ Assets/Scripts/cut/laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+  private Vector3 screenPos, objScreenPos, mousePos;
+  [SerializeField]
+  private Transform laserTransform;
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  private void OnMouseDrag() {
+    objScreenPos = Camera.main.WorldToScreenPoint(laserTransform.position);
+    mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objScreenPos.z);
+    laserTransform.position = Camera.main.ScreenToWorldPoint(mousePos);
+  }
+}
