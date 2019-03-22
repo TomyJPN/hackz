7b4fe76c523661a28cc74dedfc7b6fec2bf170b7
kamera <asahi20000124@icloud.com>
2019-03-22 11:38:21 +0900

    野菜のカット機能を追加、particleのmaterialを変更

--- Assets/Scripts/cut/CutVegetable.cs
+++ Assets/Scripts/cut/CutVegetable.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CutVegetable : MonoBehaviour
 {
-    int cutCount, cutLimit = 3;
+    int cutCount, cutLimit = 2;
     [SerializeField]
     private GameObject vegetable, cutVegetable;
+    [SerializeField]
+    private CutManeger cutManeger;
     // Start is called before the first frame update
     void Start()
     {
-        cutCount = 0;
+
     }
 
     // Update is called once per frame
@@ -27,6 +29,7 @@ public class CutVegetable : MonoBehaviour
             if (cutLimit <= cutCount)
             {
                 Instantiate(cutVegetable, vegetable.transform.position, Quaternion.identity);
+                cutManeger.CutVegetableCount++;
                 Destroy(vegetable);
             }
         }
---
+++ Assets/Scripts/cut/Cutter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutter : MonoBehaviour
+{
+    private Vector3 screenPos, objScreenPos, mousePos;
+    [SerializeField]
+    private Transform cutterTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        objScreenPos = Camera.main.WorldToScreenPoint(cutterTransform.position);
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objScreenPos.z);
+        cutterTransform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private void OnParticleTrigger()
+    {
+
+    }
+}
